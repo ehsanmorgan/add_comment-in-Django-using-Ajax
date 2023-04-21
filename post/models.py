2803fa393ef26a6e6ee53e26743d25d4e6098e64
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-04-21 22:43:46 +0200

    -

--- post/models.py
+++ post/models.py
@@ -19,6 +19,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+
     def save(self, *args, **kwargs):
        self.slug=slugify(self.title)
        super(Post, self).save(*args, **kwargs)
