ef9820a64116d322807dad2dac160789bd1d7a0a
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-07-12 15:36:43 +0200

    addd

--- post/sserializer.py
+++ post/sserializer.py
@@ -18,6 +18,9 @@ class CommentSerializer(serializers.ModelSerializer):
         
         
         
+        
+        
+        
 
 class postDetailSerializer(serializers.ModelSerializer):
     like=serializers.StringRelatedField()
