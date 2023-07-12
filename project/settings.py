bfb5aec56838004fbacec5f9176802000b47703c
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-07-12 15:37:09 +0200

    .

--- project/settings.py
+++ project/settings.py
@@ -144,6 +144,11 @@ MEDIA_ROOT=BASE_DIR/'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+
+
+
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
